55069c0b09546c4f0c099f80c20aa6af0928c548
Skander Ayachi <ayachiskander44@mail.com>
2025-08-12 05:00:39 +0200

    Version avec Dockerfile multi-stage

--- src/main/java/com/metrocal/metrocal/config/WebSecurityConfiguration.java
+++ src/main/java/com/metrocal/metrocal/config/WebSecurityConfiguration.java
@@ -55,7 +55,7 @@ public class WebSecurityConfiguration {
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(Arrays.asList(
-        "https://metrocal-gmao-9ff297.netlify.app",
+        "https://magnificent-fenglisu-a24a1c.netlify.app",
         "http://localhost:4200"
     ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
